fefd505cbd55f1073058bc71838557eb961fa966
Biel Santos <59985885+Bieelsanto@users.noreply.github.com>
2022-12-30 22:05:27 -0300

    Tentando resolver problema
    Gostaria de colocar as funções dentro das propriedades de um objeto e poder chama-las..

--- ambiente/fatoração/javascript.js
+++ ambiente/fatoração/javascript.js
@@ -3,27 +3,27 @@ function calculo(){
     var text = window.document.getElementById("texto").value.trim().replace(/ /g, '').toLowerCase()
 
     /* Função que valida se  a string está correta*/
-    valido = validarEquação(text)
+    valido = validarEquacao(text)
     if(valido){
-        /* Funções de indexação da posição de itens chaves da string */
-        som = posicaoSoma(text)
-        sub = posicaoSub(text)
-        exp = posicaoExp(text)
-        xis = posicaoXis(text)
+      /* Funções de indexação da posição de itens chaves da string */
+      som = posicaoSoma(text)
+      sub = posicaoSub(text)
+      exp = posicaoExp(text)
+      xis = posicaoXis(text)
 
-        /* Print do resultado das funções */
-        console.log(som)
-        console.log(sub)
-        console.log(exp)
-        console.log(xis)
-        console.log(valido)
+      /* Print do resultado das funções */
+      console.log(som)
+      console.log(sub)
+      console.log(exp)
+      console.log(xis)
+      console.log(valido)
     }
 }
 
-function validarEquação(text) {
-    let valido = true;
-    let temExp = false;
-    let doisXis = 0;
+function validarEquacao(text) {
+    let valido = true
+    let temExp = false
+    let doisXis = 0
     const passosDeVerificacao = {
       caracteresValidos(i) {
         if (
@@ -33,40 +33,41 @@ function validarEquação(text) {
           text[i] != "+" &&
           text[i] != "-"
         ) {
-          valido = false;
-          console.log("Utilize apenas números e os seguintes sinais: (+ - ^ x)");
-          return valido;
+          valido = false
+          console.log("Utilize apenas números e os seguintes sinais: (+ - ^ x)")
+          return
         }
       },
       segundoGrauValido(i) {
         if (
           (text[i] == "^") &&
-          (isNaN(text[i + 1]) || text[i - 1] != "x")
+          isNaN(text[i + 1]) ||
+          text[i - 1] != "x"
         ) {
-          valido = false;
-          console.log("Verifique o termo de segundo grau");
-          return valido;
+          valido = false
+          console.log("Verifique o termo de segundo grau")
+          return
         }
       },
       temExpoente(i) {
         if (text[i] == "^") {
-          temExp = true;
-          return temExp;
+          temExp = true
+          return
         }
       },
       temDoisXis(i) {
         if (text[i] == "x") {
-            doisXis = doisXis + 1;
-            console.log(doisXis);
-            return doisXis;
+            doisXis = doisXis + 1
+            console.log(doisXis)
+            return 
         }
       }
     };
     for (let i = 0; i < text.length; i++) {
-      valido = passosDeVerificacao.caracteresValidos(i);
-      valido = passosDeVerificacao.segundoGrauValido(i);
-      temExp = passosDeVerificacao.temExpoente(i);
-      doisXis =+ passosDeVerificacao.temDoisXis(i);
+      passosDeVerificacao.caracteresValidos(i);
+      passosDeVerificacao.segundoGrauValido(i);
+      passosDeVerificacao.temExpoente(i);
+      passosDeVerificacao.temDoisXis(i);
       console.log(doisXis);
     }
     if (!temExp) {
